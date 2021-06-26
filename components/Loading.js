5e509bd66577addd0907d94aeec65abd1486b30e
Nick Hale <datfuriouskid@live.com>
2021-06-25 21:06:45 -0700

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,11 @@
 import { Circle } from 'better-react-spinkit';
+import Image from "next/image";
 
 function Loading() {
     return (
       <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
         <div>
-          <img
+          <Image
             src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
             alt=""
             style={{ marginBottom: 10 }}
